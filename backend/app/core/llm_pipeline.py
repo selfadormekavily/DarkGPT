40f510d1307dc9b88c3362836b845a0e93cf963f
selfadormekavily <133525095+selfadormekavily@users.noreply.github.com>
2026-09-18 16:27:47 +0300

    Update llm_pipeline.py

--- backend/app/core/llm_pipeline.py
+++ backend/app/core/llm_pipeline.py
@@ -4,7 +4,7 @@ from langchain_groq import ChatGroq
 
 def process_query(query):
     model = ChatGroq(
-        model="qwen/qwen3.6-27b",
+        model="openai/gpt-oss-20b",
         api_key=os.environ.get("GROQ_API_KEY"),
         temperature=0.7,
     )
